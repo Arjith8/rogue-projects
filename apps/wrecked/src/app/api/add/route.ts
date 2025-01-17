fc137ebc8687758d814bab6709a2c6f6acc7abee
Arjithar8 <arjithar8@gmail.com>
2025-01-17 20:55:39 +0530

    Checking if connection is proper

--- apps/wrecked/src/app/api/add/route.ts
+++ apps/wrecked/src/app/api/add/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest } from "next/server";
 
 export async function POST(request: NextRequest){
-    console.log(request)
+    console.log(request.json())
     return Response.json({hello:"hello"})
 }
